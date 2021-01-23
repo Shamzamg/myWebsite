0ca1547f0f2d6705c446c7aa721a0af37b87d43b
p1718423 <shamsdine.ziani@etu.univ-lyon1.fr>
2021-01-23 01:05:23 +0100

    left part almost done, button left

--- src/components/Timeline/CustomTimeline.js
+++ src/components/Timeline/CustomTimeline.js
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import { Typography } from '@material-ui/core';
 import Timeline from '@material-ui/lab/Timeline';
 import TimelineItem from '@material-ui/lab/TimelineItem';
 import TimelineSeparator from '@material-ui/lab/TimelineSeparator';
@@ -7,34 +8,38 @@ import TimelineConnector from '@material-ui/lab/TimelineConnector';
 import TimelineContent from '@material-ui/lab/TimelineContent';
 import TimelineDot from '@material-ui/lab/TimelineDot';
 
+import WorkIcon from '@material-ui/icons/Work';
+
 import './Timeline.css';
 
-const CustomTimeline = () => {
+const CustomTimeline = ({title, icon, children}) => {
     return (
-        <Timeline>
-        <TimelineItem>
+        <Timeline className='timeline'>
+        {/*First Item */}
+        <TimelineItem className='timeline_firstItem'>
           <TimelineSeparator>
-            <TimelineDot />
+            <TimelineDot className='timeline_dot_header'>
+              {icon}
+            </TimelineDot>
             <TimelineConnector />
           </TimelineSeparator>
-          <TimelineContent>Eat</TimelineContent>
+          <TimelineContent><Typography variant='h6' className='timeline_header'>{title}</Typography></TimelineContent>
         </TimelineItem>
-        <TimelineItem>
-          <TimelineSeparator>
-            <TimelineDot />
-            <TimelineConnector />
-          </TimelineSeparator>
-          <TimelineContent>Code</TimelineContent>
-        </TimelineItem>
-        <TimelineItem>
-          <TimelineSeparator>
-            <TimelineDot />
-          </TimelineSeparator>
-          <TimelineContent>Sleep</TimelineContent>
-        </TimelineItem>
+
+        {children}
+
       </Timeline>
     )
 }
+
+export const CustomTimelineSeparator = () => (
+    <TimelineSeparator className='separator_padding'>
+      <TimelineDot variant='outlined' className='timeline_dot' />
+      <TimelineConnector />
+    </TimelineSeparator>
+);
+
+
 
 export default CustomTimeline
 
---
+++ src/utils/resumeData.js
@@ -0,0 +1,37 @@
+import FacebookIcon from '@material-ui/icons/Facebook';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import GitHubIcon from '@material-ui/icons/GitHub';
+
+export default {
+    name: 'Shams',
+    title: 'IT Engineer student',
+    birthday: '23th November 2000',
+    email: 'shamsdine.ziani@etu.univ-lyon1.fr',
+    address: '7 rue Dedieu, 69100 Villeurbanne',
+    phone: '+33 7 69 64 59 61',
+
+    socials: {
+        Facebook: {
+            link: '',
+            text: 'shamsFacebook',
+            icon: FacebookIcon
+        },
+        Twitter: {
+            link: 'https://twitter.com/ChiraiiTv',
+            text: 'shamsTwitter',
+            icon: TwitterIcon
+        },
+        Github: {
+            link: '',
+            text: 'shamsLinkedIn',
+            icon: LinkedInIcon
+        },
+        Linkedin: {
+            link: '',
+            text: 'shamsGitHub',
+            icon: GitHubIcon
+        },
+    }
+
+}
